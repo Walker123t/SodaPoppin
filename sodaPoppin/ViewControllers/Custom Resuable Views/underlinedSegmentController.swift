5b431444322577baa2c4f923278f8f0c8deeba8f
Trevor Walke <Walker123t@gmail.com>
2019-08-23 14:46:49 -0600

    added cool things

--- sodaPoppin/ViewControllers/Custom Resuable Views/underlinedSegmentController.swift
+++ sodaPoppin/ViewControllers/Custom Resuable Views/underlinedSegmentController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
+@IBDesignable
 class underlinedSegmentController: UIView {
     var buttons: [UIButton] = []
     var selector: UIView!
     
     @IBInspectable
-    var lineheight: CGFloat = 3 {
+    var fontSize: Int = 20 {
         didSet {
             updateView()
         }
     }
     @IBInspectable
-    var lineWidth: CGFloat = 5 {
+    var lineheight: CGFloat = 5 {
         didSet {
             updateView()
         }
@@ -33,7 +34,7 @@ class underlinedSegmentController: UIView {
     }
     
     @IBInspectable
-    var textColor: UIColor = .black {
+    var textColor: UIColor = .white {
         didSet {
             updateView()
         }
@@ -46,12 +47,6 @@ class underlinedSegmentController: UIView {
         }
     }
     
-    @IBInspectable
-    var selectorTextColor: UIColor = .purple {
-        didSet {
-            updateView()
-        }
-    }
     func updateView() {
         buttons = []
         subviews.forEach {$0.removeFromSuperview()}
@@ -64,10 +59,9 @@ class underlinedSegmentController: UIView {
             button.addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
             buttons.append(button)
         }
-        buttons[0].setTitleColor(.purple, for: .normal)
         
         let selectorWidth = frame.width / CGFloat(buttonTitles.count)
-        selector = UIView(frame: CGRect(x: 0, y: 0, width: selectorWidth, height: frame.height))
+        selector = UIView(frame: CGRect(x: 0, y: self.frame.height, width: selectorWidth, height: lineheight))
         selector.backgroundColor = selectorColor
         addSubview(selector)
         let sv = UIStackView(arrangedSubviews: buttons)
@@ -81,27 +75,19 @@ class underlinedSegmentController: UIView {
         sv.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         sv.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
     }
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-        
-    }
     
     @objc func buttonTapped(button: UIButton) {
         for (buttonIndex, btn) in buttons.enumerated() {
-            btn.setTitleColor(textColor, for: .normal)
-            
+            btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: CGFloat(fontSize))
+            btn.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(fontSize), weight: .regular)
             if btn == button {
                 let selectorStartPosition = frame.width / CGFloat(buttons.count) * CGFloat(buttonIndex)
                 UIView.animate(withDuration: 0.2) {
                     self.selector.frame.origin.x = selectorStartPosition
                 }
-                btn.setTitleColor(selectorTextColor, for: .normal)
+                btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: CGFloat(fontSize))
             }
         }
     }
     
-}
-
 }
