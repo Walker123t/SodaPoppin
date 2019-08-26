d4e7620f74435404a7f042403c459de64e9f316b
Nic Gibson <nic@shpangy.com>
2019-08-26 11:41:38 -0600

    Worked on tab 1 views

---
+++ sodaPoppin/ViewControllers/Tab 1 View Controllers/DiscoverPoppinViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DiscoverPoppinViewController.swift
+//  sodaPoppin
+//
+//  Created by Nic Gibson on 8/26/19.
+//  Copyright © 2019 Trevor Walker. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverPoppinViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
