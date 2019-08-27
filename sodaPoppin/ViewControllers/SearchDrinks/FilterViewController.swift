ebb496576dd85a57c494089c8eb7f9d6aae3bd8a
Trevor Walke <Walker123t@gmail.com>
2019-08-27 09:55:21 -0600

    Daily Commit

--- sodaPoppin/ViewControllers/SearchDrinks/FilterViewController.swift
+++ sodaPoppin/ViewControllers/SearchDrinks/FilterViewController.swift
@@ -6,33 +6,33 @@
 //  Copyright © 2019 Trevor Walker. All rights reserved.
 //
 
-import UIKit
-
-class FilterViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+//import UIKit
+//
+//class FilterViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
